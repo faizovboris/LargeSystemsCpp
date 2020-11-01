e1c717f0200750c8a524db172426f85c712a4304
Faizov Boris <faizov_boris@mail.ru>
2020-11-01 16:34:59 +0300

    Решение 5-го необязательного домашнего задания

---
+++ additional_task_5/intrusive_ptr.h
@@ -0,0 +1,118 @@
+#include <algorithm>
+
+template<typename T>
+class TBasePtr {
+public:
+    TBasePtr(T* from_ptr = nullptr) : ptr(from_ptr) { }
+
+    T* operator->() const {
+        return ptr;
+    }
+
+    T& operator*() const {
+        return *ptr;
+    }
+
+    bool operator==(const TBasePtr& other) const {
+        return ptr == other.ptr;
+    }
+
+    bool operator!=(const TBasePtr& other) const {
+        return !this->operator==(other);
+    }
+
+    operator bool() const {
+        return ptr != nullptr;
+    }
+
+protected:
+    T* ptr;
+};
+
+template<typename T>
+class TIntrusivePtr : public TBasePtr<T> {
+public:
+    TIntrusivePtr(T* from_ptr) : TBasePtr<T>(from_ptr) {
+        if (this->ptr) {
+            this->ptr->Inc();
+        }
+    }
+
+    TIntrusivePtr(const TIntrusivePtr& other) : TIntrusivePtr<T>(other.ptr) { }
+
+    TIntrusivePtr(TIntrusivePtr&& other) noexcept : TIntrusivePtr<T>(other.ptr) {
+        other.Release();
+    }
+
+    TIntrusivePtr& operator=(TIntrusivePtr other) {
+        Swap(other);
+        return *this;
+    }
+
+    ~TIntrusivePtr() {
+        if (this->ptr) {
+            this->ptr->Dec();
+            if (this->ptr->RefCount() == 0) {
+                delete(this->ptr);
+            }
+        }
+    }
+
+    size_t UseCount() const {
+        if (this->ptr) {
+            return this->ptr->RefCount();
+        } else {
+            return 0;
+        }
+    }
+
+    T* Get() const {
+        return this->ptr;
+    }
+
+    void Reset(TIntrusivePtr<T> other = nullptr) {
+        Swap(other);
+    }
+
+    void Release() {
+        Reset(nullptr);
+    }
+
+    void Swap(TIntrusivePtr<T>& other) {
+        std::swap(this->ptr, other.ptr);
+    }
+
+    size_t RefCount() const {
+        return UseCount();
+    }
+};
+
+template <typename T, typename... Args>
+TIntrusivePtr<T> MakeIntrusive(Args&&... params) {
+    return TIntrusivePtr<T>(new T(std::forward<Args>(params)...));
+}
+
+template <typename T>
+class TRefCounter
+{
+public:
+    TRefCounter() = default;
+
+    size_t RefCount() {
+        return ref_count;
+    }
+
+private:
+    friend TIntrusivePtr<T>;
+
+    size_t Dec() {
+        --ref_count;
+        return ref_count;
+    }
+
+    size_t Inc() {
+        ++ref_count;
+        return ref_count;
+    }
+    size_t ref_count = 0;
+};
---
+++ additional_task_5/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cassert>
+#include "intrusive_ptr.h"
+
+class TDoc: public TRefCounter<TDoc> {
+// ...
+};
+
+int main() {
+    TIntrusivePtr<TDoc> ptr = nullptr;
+    ptr = MakeIntrusive<TDoc>();
+    assert(ptr.RefCount() == 1);
+
+    TIntrusivePtr<TDoc> ptr2 = ptr;
+    assert((ptr.RefCount() == 2) && (ptr.Get() == ptr2.Get()));
+
+    TIntrusivePtr<TDoc> ptr3 = MakeIntrusive<TDoc>();
+    ptr3.Reset(ptr2);
+    assert((ptr.RefCount() == 3) && (ptr2.RefCount() == 3) && (ptr3.RefCount() == 3) && 
+           (ptr.Get() == ptr2.Get()) && (ptr.Get() == ptr3.Get()));
+
+    ptr3.Reset();
+    assert((ptr.RefCount() == 2) && (ptr2.RefCount() == 2) && (ptr3.RefCount() == 0) && 
+           (ptr.Get() == ptr2.Get()) && (ptr3.Get() == nullptr));
+
+    ptr3.Reset(std::move(ptr2));
+    assert((ptr.RefCount() == 2) && (ptr2.RefCount() == 0) && (ptr3.RefCount() == 2) && 
+           (ptr.Get() == ptr3.Get()) && (ptr2.Get() == nullptr));
+
+    static_assert(sizeof(TDoc*) == sizeof(ptr));
+    std::cerr << "ni\n";
+}
